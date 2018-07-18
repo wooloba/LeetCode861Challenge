3053d4b5caeedaf69f06927afe4a4296b3872a66
YAOZHI LU <yaozhilu@YAOZHI-MacBook-Pro.local>
2018-07-18 23:26:50 +0800

    edit 684

--- 684.findRedundantConnection.py
+++ 684.findRedundantConnection.py
@@ -8,10 +8,15 @@
 
 class Solution(object):
     def findRedundantConnection(self, edges):
+        nodes = list(set(sum(edges,[])))
+        nodes = dict.fromkeys(nodes, 0)
         
+        
+
+
 def main():
     solution = Solution()
-    print solution.findRedundantConnection(edges)
+    print solution.findRedundantConnection([[1,2], [2,3], [3,4], [1,4], [1,5]])
 
 
 if __name__ == "__main__":
