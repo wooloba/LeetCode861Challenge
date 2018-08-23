1bcf482b92e6902153560e22b7f9bc896ad475a2
YAOZHI LU <luyaozhiusing@gmail.com>
2018-08-23 11:22:35 -0700

    559

---
+++ 559. Maximum Depth of N-ary Tree.py
@@ -0,0 +1,35 @@
+####################
+#   Yaozhi Lu      #
+#   Aug 23 2018    #
+####################
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+
+        if not root:
+            return 0
+        if not root.children:
+            return 1
+        else:
+            return max(self.maxDepth(node) for node in root.children) + 1
+
+def main():
+    so = Solution()
+    
+    print(so.maxDepth(root))
+
+
+if __name__ == '__main__':
+    main()
+
