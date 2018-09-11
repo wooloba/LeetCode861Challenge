fc4b760a7093430b65cf0a824cbc9efb8af406f2
YAOZHI LU <luyaozhiusing@gmail.com>
2018-09-10 21:20:44 -0600

    821

--- 821. Shortest Distance to a Character.py
+++ 821. Shortest Distance to a Character.py
@@ -19,26 +19,22 @@ class Solution(object):
         for i,e in enumerate(S):
             if e == C:
                 result[i] = 0
-                for j in range(leng-1,-1,-1):
-                    if (leng - j) < result[(i-j-1)]:
-                        result[(i-j-1)] = leng - j
-                leng = 0 
+                for j in range(i-leng,i):
+                    result[j] = leng
+                    leng -= 1
             else:
                 leng += 1
-        print result
-        
+        print result    
+
         result = result[::-1]
         leng = 0
         for i,e in enumerate(S[::-1]):
-
             if e == C:
-                print leng
-                print 'aaa'
-                for j in range(leng-1,-1,-1):
-                    print j 
-                    if (leng - j) < result[(i-j-1)]:
-                        result[(i-j-1)] = j+1
-                leng = 0 
+                k = 1
+                for j in range(i-1,i-leng-1,-1):
+                    if k < result[j]:
+                        result[j] = k
+                    k += 1
             else:
                 leng += 1
 
@@ -47,7 +43,7 @@ class Solution(object):
 
 def main():
     so = Solution()
-    A = 'eeeeeeeeeloveleetcode'
+    A = 'loveleetcode'
     C = 'l'
     
     print so.shortestToChar(A,C)
